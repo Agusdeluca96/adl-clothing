ef224bda3f1a1c32924f66c65087419040af7b92
Agustin De Luca <agusdeluca96@gmail.com>
2020-03-23 14:05:40 -0300

    Changed Directory images

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -9,32 +9,32 @@ const Directory = () => {
     {
       id: 1,
       title: 'hats',
-      imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+      imageUrl: 'https://i.ibb.co/9HcKSRr/hats.jpg',
       linkUrl: 'shop/hats'
     },
     {
       id: 2,
       title: 'jackets',
-      imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+      imageUrl: 'https://i.ibb.co/h2ZBvMH/jackets.jpg',
       linkUrl: 'shop/jackets'
     },
     {
       id: 3,
       title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+      imageUrl: 'https://i.ibb.co/FJ5Hdqq/sneakers.jpg',
       linkUrl: 'shop/sneakers'
     },
     {
       id: 4,
       title: 'womens',
-      imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+      imageUrl: 'https://i.ibb.co/98PR8qm/women.jpg',
       size: 'large',
       linkUrl: 'shop/womens'
     },
     {
       id: 5,
       title: 'mens',
-      imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+      imageUrl: 'https://i.ibb.co/jZZ4QgN/men.jpg',
       size: 'large',
       linkUrl: 'shop/mens'
     }
