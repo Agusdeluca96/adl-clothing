94a14fe1ece1b103d0f95f0b381a85fa70065da8
Agustin De Luca <agusdeluca96@gmail.com>
2020-03-24 02:40:10 -0300

    Added react router dom, and modified MenuItem component to use withRoute high order component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import './App.css';
+import { Switch, Route } from 'react-router-dom'
+
 import Homepage from './pages/Homepage';
+import './App.css';
 
 function App() {
   return (
     <div>
+      <Switch></Switch>
       <Homepage />
     </div>
   );
--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom'
 
 import './MenuItem.scss'
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`menu-item ${size}`}>
-    <div className="background-image"
-      style={{ backgroundImage: `url(${imageUrl})` }} />
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div className={`menu-item ${size}`} onClick={() => history.match.push(`${match.url}${linkUrl}`)}>
+    <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="content">
       <span className="title">{title.toUpperCase()}</span>
       <span className="subtitle">SHOP NOW</span>
@@ -13,4 +13,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
   </div>
 )
 
-export default MenuItem;
+export default withRouter(MenuItem);
