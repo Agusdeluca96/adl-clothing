86aa181e9b33f2ae30f92b358400840fd2a35386
Agustin De Luca <agusdeluca96@gmail.com>
2020-03-24 03:21:47 -0300

    Refactoring on data array of directories

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -2,45 +2,11 @@ import React, { useState } from 'react';
 import MenuItem from '../../components/MenuItem';
 
 import './Directory.scss'
+import DIRECTORY_DATA from './Directory.data';
 
 const Directory = () => {
 
-  const sectionsData = [
-    {
-      id: 1,
-      title: 'hats',
-      imageUrl: 'https://i.ibb.co/9HcKSRr/hats.jpg',
-      linkUrl: 'shop/hats'
-    },
-    {
-      id: 2,
-      title: 'jackets',
-      imageUrl: 'https://i.ibb.co/h2ZBvMH/jackets.jpg',
-      linkUrl: 'shop/jackets'
-    },
-    {
-      id: 3,
-      title: 'sneakers',
-      imageUrl: 'https://i.ibb.co/FJ5Hdqq/sneakers.jpg',
-      linkUrl: 'shop/sneakers'
-    },
-    {
-      id: 4,
-      title: 'womens',
-      imageUrl: 'https://i.ibb.co/98PR8qm/women.jpg',
-      size: 'large',
-      linkUrl: 'shop/womens'
-    },
-    {
-      id: 5,
-      title: 'mens',
-      imageUrl: 'https://i.ibb.co/jZZ4QgN/men.jpg',
-      size: 'large',
-      linkUrl: 'shop/mens'
-    }
-  ];
-
-  const [sections, setSections] = useState(sectionsData);
+  const [sections, setSections] = useState(DIRECTORY_DATA);
   const handleSectionsChange = e => setSections(e.target.value);
 
   return (
