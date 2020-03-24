0821bed7989490460a96c40e44373fa3a14ffe65
Agustin De Luca <agusdeluca96@gmail.com>
2020-03-24 03:20:13 -0300

    Configurated routes and added shop page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
 
-import Homepage from './pages/Homepage';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Switch></Switch>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/shop" component={Shop} />
+      </Switch>
     </div>
   );
 }
