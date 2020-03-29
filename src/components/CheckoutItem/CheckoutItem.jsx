6eba1de461808558300f54c7350f77532f75778f
Agustin De Luca <agusdeluca96@gmail.com>
2020-03-29 06:10:10 -0300

    Integrated addItem and removeItem into CheckoutItem

--- src/components/CheckoutItem/CheckoutItem.jsx
+++ src/components/CheckoutItem/CheckoutItem.jsx
@@ -1,28 +1,40 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { removeItem } from "../../redux/cart/cart.actions";
+import { addItem, removeItem, clearItem } from "../../redux/cart/cart.actions";
 
 import './CheckoutItem.scss';
 
-const CheckoutItem = ({ cartItem, removeItem }) => (
+const CheckoutItem = ({ cartItem, addItem, removeItem, clearItem }) => (
   <div className="checkout-item">
     <div className="image-container">
       <img alt="item" src={cartItem.imageUrl} />
     </div>
     <span className="name">{cartItem.name}</span>
-    <span className="quantity">{cartItem.quantity}</span>
+    <span className="quantity">
+      <div
+        className="quantity-arrow"
+        onClick={() => removeItem(cartItem)}
+      >&#10094;</div>
+      <div className="quantity-value">{cartItem.quantity}</div>
+      <div
+        className="quantity-arrow"
+        onClick={() => addItem(cartItem)}
+      >&#10095;</div>
+    </span>
     <span className="price">${cartItem.price}</span>
     <span
       className="remove-button"
-      onClick={() => removeItem(cartItem)}
+      onClick={() => clearItem(cartItem)}
     >&#10007;</span>
   </div>
 );
 
 
 const mapDispatchToProps = (dispatch) => ({
-  removeItem: item => dispatch(removeItem(item))
+  addItem: item => dispatch(addItem(item)),
+  removeItem: item => dispatch(removeItem(item)),
+  clearItem: item => dispatch(clearItem(item))
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
