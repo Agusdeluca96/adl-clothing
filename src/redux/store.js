f34c8f52704523d2bddfbf633101fec8e737a4ab
Agustin De Luca <agusdeluca96@gmail.com>
2020-04-05 05:27:06 -0300

    Modified middleware to remove logger from prod

--- src/redux/store.js
+++ src/redux/store.js
@@ -6,6 +6,10 @@ import reducer from './reducer';
 
 const middlewares = [logger];
 
+if (process.env.NODE_ENV === 'development') {
+  middlewares.push(logger);
+}
+
 export const store = createStore(reducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
