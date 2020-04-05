ba6bf9d9775006e5cf7d74fb9fd5ca53e2175cd8
Agustin De Luca <agusdeluca96@gmail.com>
2020-04-04 21:39:22 -0300

    Fix into routing push in MenuItem

--- src/components/MenuItem/MenuItem.jsx
+++ src/components/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ import { withRouter } from 'react-router-dom'
 import './MenuItem.scss'
 
 const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
-  <div className={`menu-item ${size}`} onClick={() => history.match.push(`${match.url}${linkUrl}`)}>
+  <div className={`menu-item ${size}`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image" style={{ backgroundImage: `url(${imageUrl})` }} />
     <div className="content">
       <span className="title">{title.toUpperCase()}</span>
