225df04bd3a1702d456ccb7a2941d0e1c5654d66
Agustin De Luca <agusdeluca96@gmail.com>
2020-04-14 21:06:38 -0300

    Created base shopSaga function to export calls

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,11 +1,11 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { all, takeLatest, call, put } from "redux-saga/effects";
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../utils/firebase';
 
 import ShopActionTypes from './shop.types';
 import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions';
 
-export function* fetchCollectionsAsync() {
+export function* fetchCollections() {
   try {
     const collectionRef = firestore.collection('collections');
     const collectionsSnapshot = yield collectionRef.get();
@@ -18,9 +18,12 @@ export function* fetchCollectionsAsync() {
   }
 };
 
-export function* fetchCollectionsStart() {
-  yield takeLatest(
-    ShopActionTypes.FETCH_COLLECTIONS_START,
-    fetchCollectionsAsync
-  );
+export function* onFetchCollectionsStart() {
+  yield takeLatest(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollections);
+}
+
+export function* shopSagas() {
+  yield all([
+    call(onFetchCollectionsStart)
+  ])
 }
