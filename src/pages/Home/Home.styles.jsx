a570cc42d26974875df686ec4389b2fff8727833
Agustin De Luca <agusdeluca96@gmail.com>
2020-05-25 05:47:47 -0300

    Fixed styling of content container

--- src/pages/Home/Home.styles.jsx
+++ src/pages/Home/Home.styles.jsx
@@ -1,15 +1,5 @@
 import styled from 'styled-components';
 
 export const HomeContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-
-  @media screen and (min-width: 800px) {
-    height: -moz-available;          /* WebKit-based browsers will ignore this. */
-    height: -webkit-fill-available;  /* Mozilla-based browsers will ignore this. */
-    height: fill-available;
-  }
+  width: 100%;
 `;
